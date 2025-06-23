68b4c7f4824d23a75787213d977ba302f0feb2ea
Ekta-pawar <ektapawar0001@gmail.com>
2025-06-23 15:35:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let data = document.getElementById("timer");
+
+function updateClock() {
+    const date = new Date();
+    const formattedDate = date.toLocaleString();
+    data.innerHTML = formattedDate;
+ }
+
+updateClock();
+setInterval(updateClock, 1000);
